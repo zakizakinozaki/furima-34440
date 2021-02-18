65ae37c396477b344c9b60bbcd1824cb313fe0db
zakizakinozaki <78135984+zakizakinozaki@users.noreply.github.com>
2021-02-18 19:22:37 +0900

    Revert "Revert "productモデル作成、マイグレーション実行""
    This reverts commit 76c08ba939bac3bad08212d69a50ecc46af92068.

---
+++ db/migrate/20210218064504_create_products.rb
@@ -0,0 +1,17 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      t.string :name,                   null: false, default: ''
+      t.text :description,              null: false
+      t.integer :price,                 null: false
+      t.integer :category_id,           null: false
+      t.integer :status_id,             null: false
+      t.integer :cost_id,               null: false
+      t.integer :state_id,              null: false
+      t.integer :date_id,               null: false
+      t.references :user,               foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_16_101024) do
+ActiveRecord::Schema.define(version: 2021_02_18_064504) do
+
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.text "description", null: false
+    t.integer "price", null: false
+    t.integer "category_id", null: false
+    t.integer "status_id", null: false
+    t.integer "cost_id", null: false
+    t.integer "state_id", null: false
+    t.integer "date_id", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", default: "", null: false
@@ -30,4 +45,5 @@ ActiveRecord::Schema.define(version: 2021_02_16_101024) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "users"
 end
