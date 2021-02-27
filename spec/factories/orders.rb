aad622acc6c3dff67c8a5beade2573a010a468f1
zakizakinozaki <78135984+zakizakinozaki@users.noreply.github.com>
2021-02-27 12:30:48 +0900

    orderモデルの単体テスト実装

---
+++ spec/factories/orders.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :order do
+    postal_code      { '123-4567' }
+    state_id         { 2 }
+    city             { 'test' }
+    address          { 'test' }
+    phone_num        { 111_111_111 }
+    token            { 'tok_abcdefghijk00000000000000000' }
+  end
+end
